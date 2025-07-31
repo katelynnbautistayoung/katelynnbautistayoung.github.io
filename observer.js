cecb58255b7dabdf53609979786c69227a355df8
katelynnbautistayoung <katelynnbautistayoung@gmail.com>
2025-07-31 16:00:43 +0800

    Update observer.js

--- observer.js
+++ observer.js
@@ -1,21 +1,21 @@
 const observer = new IntersectionObserver((entries) => {
 	entries.forEach((entry) => {
 		if (entry.isIntersecting) {
-			entry.target.classList.add("in-view")
-			entry.target.classList.remove("not-in-view")
+			entry.target.classList.add("in-view");
+			entry.target.classList.remove("not-in-view");
 		} else {
-		entry.target.classList.remove("in-view")
-		entry.target.classList.add("not-in-view")
+		entry.target.classList.remove("in-view");
+		entry.target.classList.add("not-in-view");
 		}
-	})
+	});
 }, {
 	rootMargin: "0px",
 	threshold: [0, 0.1, 1]
 	},
-)
+);
 
-const tags = document.querySelectorAll("figure, div, abouttitle, abouttext, card, centercard, about")
+const tags = document.querySelectorAll("figure, div");
 
 tags.forEach((tag) => {
-	observer.observe("figure, div, abouttitle, abouttext, card, centercard, about")
-})
+	observer.observe("figure, div");
+});
